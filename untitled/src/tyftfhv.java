81e00d6b3a160dc37279f021a96f6cf279c890bc
student <mostalex@mail.ru>
2013-09-16 15:57:39 +0400

    It is our first comment.

---
+++ untitled/src/tyftfhv.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: student
+ * Date: 16.09.13
+ * Time: 15:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class tyftfhv
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
